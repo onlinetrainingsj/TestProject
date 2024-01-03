a569bac60670d2661e99ebf21e41262f17197068
bhara <bhara@DESKTOP-17LHQBD>
2024-01-03 22:52:34 +0530

    ABCDE

--- testABCD/src/testABCD/TestClass.java
+++ testABCD/src/testABCD/TestClass.java
@@ -1,7 +1,5 @@
 package testABCD;
 
-import org.testng.annotations.Test;
-
 public class TestClass {
 
 	public static void main(String[] args) {
@@ -9,11 +7,6 @@ public class TestClass {
 		System.out.println("Hello");
 		System.out.println("Hello ABCD");
 		System.out.println("Hello Child Branch");
-	}
-	
-	@Test
-	public void a() {
-		System.out.println("Helo TestNG");
 	}
 
 }
